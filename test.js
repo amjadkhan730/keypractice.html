b63fce06b5195097970f40efd67877b0b33ed741
amjadkhan730 <amjadktk730@gmail.com>
2022-12-18 12:39:35 +0500

    new adding in array method

--- test.js
+++ test.js
@@ -1,152 +1,171 @@
-// // // // // // // // // // // // // // // // // console.log("testing....");
-// // // // // // // // // // // // // // // // // console.log("testing....");
-// // // // // // // // // // // // // // // // // console.log("testing....");
-// // // // // // // // // // // // // // // // // console.log("testing....");
-// // // // // // // // // // // // // // // // // console.log("testing....");
-// // // // // // // // // // // // // // // // // console.log("testing....");
-// // // // // // // // // // // // // // // // // console.log("testing....");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
-// // // // // // // // // // // // // // // // // console.log("Tufail Zaman");        ``
+// // // // // // // // // // // // // // // // // // // // console.log("testing....");
+// // // // // // // // // // // // // // // // // // // // console.log("testing....");
+// // // // // // // // // // // // // // // // // // // // console.log("testing....");
+// // // // // // // // // // // // // // // // // // // // console.log("testing....");
+// // // // // // // // // // // // // // // // // // // // console.log("testing....");
+// // // // // // // // // // // // // // // // // // // // console.log("testing....");
+// // // // // // // // // // // // // // // // // // // // console.log("testing....");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");
+// // // // // // // // // // // // // // // // // // // // console.log("Tufail Zaman");        ``
 
 
-// // // // // // // // // // // // // // // // const mark = 20;
-// // // // // // // // // // // // // // // // if(mark<15){
-// // // // // // // // // // // // // // // //     console.log ("U r fail")
+// // // // // // // // // // // // // // // // // // // const mark = 20;
+// // // // // // // // // // // // // // // // // // // if(mark<15){
+// // // // // // // // // // // // // // // // // // //     console.log ("U r fail")
+// // // // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // // // else{
+// // // // // // // // // // // // // // // // // // //     console.log ("u r pass")
+// // // // // // // // // // // // // // // // // // // }
+
+// // // // // // // // // // // // // // // // // // const marks = 50;
+// // // // // // // // // // // // // // // // // // if(marks<30 && marks>=0){
+// // // // // // // // // // // // // // // // // // console.log("you have pass")
+// // // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // // if(marks>33 && marks<=50){
+// // // // // // // // // // // // // // // // // //     console.log("u r pass")
+// // // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // // else{
+// // // // // // // // // // // // // // // // // //     console.log("u r fail")
+// // // // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // // const marks = 50;
+// // // // // // // // // // // // // // // // // (marks>=33 ? console.log("pass"): console.log("fail"))
+// // // // // // // // // // // // // // // // const num = 40
+// // // // // // // // // // // // // // // // switch(num){
+
+// // // // // // // // // // // // // // // //     case 40:
+// // // // // // // // // // // // // // // //         console.log("yss")
+// // // // // // // // // // // // // // // //         break;
+// // // // // // // // // // // // // // // //  case 20:
+// // // // // // // // // // // // // // // //     console.log("NO")
+// // // // // // // // // // // // // // // //     break;
+// // // // // // // // // // // // // // // //     default:
+// // // // // // // // // // // // // // // //         console.log("default")
 // // // // // // // // // // // // // // // // }
-// // // // // // // // // // // // // // // // else{
-// // // // // // // // // // // // // // // //     console.log ("u r pass")
-// // // // // // // // // // // // // // // // }
 
-// // // // // // // // // // // // // // // const marks = 50;
-// // // // // // // // // // // // // // // if(marks<30 && marks>=0){
-// // // // // // // // // // // // // // // console.log("you have pass")
+
+// // // // // // // // // // // // // // // for(let i=0; i<10; i++){
+// // // // // // // // // // // // // // //     console.log(i);
 // // // // // // // // // // // // // // // }
-// // // // // // // // // // // // // // // if(marks>33 && marks<=50){
-// // // // // // // // // // // // // // //     console.log("u r pass")
+// // // // // // // // // // // // // // // for(i=0; i<10; console.log(i)){
+// // // // // // // // // // // // // // //     i++
 // // // // // // // // // // // // // // // }
-// // // // // // // // // // // // // // // else{
-// // // // // // // // // // // // // // //     console.log("u r fail")
-// // // // // // // // // // // // // // // }
-// // // // // // // // // // // // // // const marks = 50;
-// // // // // // // // // // // // // // (marks>=33 ? console.log("pass"): console.log("fail"))
-// // // // // // // // // // // // // const num = 40
-// // // // // // // // // // // // // switch(num){
 
-// // // // // // // // // // // // //     case 40:
-// // // // // // // // // // // // //         console.log("yss")
-// // // // // // // // // // // // //         break;
-// // // // // // // // // // // // //  case 20:
-// // // // // // // // // // // // //     console.log("NO")
-// // // // // // // // // // // // //     break;
-// // // // // // // // // // // // //     default:
-// // // // // // // // // // // // //         console.log("default")
-// // // // // // // // // // // // // }
+// // // // // // // // // // // //          //////// Array Method//////////
 
 
-// // // // // // // // // // // // for(let i=0; i<10; i++){
-// // // // // // // // // // // //     console.log(i);
-// // // // // // // // // // // // }
-// // // // // // // // // // // // for(i=0; i<10; console.log(i)){
-// // // // // // // // // // // //     i++
-// // // // // // // // // // // // }
-
-// // // // // // // // //          //////// Array Method//////////
-
-
-// // // // // // // // // forEach:
-// // // // // // // // //          Read one by one elelement in array 
+// // // // // // // // // // // // forEach:
+// // // // // // // // // // // //          Read one by one elelement in array 
 
 
 
-// // // // // // // // const arr = [1,2,3,4,5,6,7,8,9];
-// // // // // // // // arr.forEach(item=>{
-// // // // // // // //     console.log(item);
-// // // // // // // // })
+// // // // // // // // // // // const arr = [1,2,3,4,5,6,7,8,9];
+// // // // // // // // // // // arr.forEach(item=>{
+// // // // // // // // // // //     console.log(item);
+// // // // // // // // // // // })
 
 
-// // // // // // // ////inclide////
-// // // // // // //                 //this method check the item present in array.
+// // // // // // // // // // ////inclide////
+// // // // // // // // // //                 //this method check the item present in array.
 
-// // // // // // //          const arr = [1,2,3,4,5,6,7,8,9];
-// // // // // // //          //arr.includes(5);
-// // // // // // //          let getvaluepresentornot = arr.includes(6);
-// // // // // // //          console.log(getpresentornot);       
-
-
-// // // // // // ///////Filter///
-// // // // // //                  //this method create new array with only element passed condition,,,,Filter always reurn in array.
-                 
-// // // // //            const arr = [1,2,3,4,5,6,7,8,9];
-// // // // //             const arrfilter = arr.filter(num => num<=6);
-// // // // //             console.log(arrfilter);
-                 
-
-// // // // // //////map///
-// // // // //               //this method create new array by calling the providing function in every element.
-              
-// // // // //    //           const arr=[1,2,3,4,5,6,7,8,9];
-// // // // //      //         const oneAdded = arr.map(num => num+2);
-// // // // //        //       console.log(oneAdded);
+// // // // // // // // // //          const arr = [1,2,3,4,5,6,7,8,9];
+// // // // // // // // // //          //arr.includes(5);
+// // // // // // // // // //          let getvaluepresentornot = arr.includes(6);
+// // // // // // // // // //          console.log(getpresentornot);       
 
 
-// // // // //////Reduce///
-// // // //                 // This method applies a function an accumlator and each element it the array from (left to right) to reduce it to a single value.
+// // // // // // // // // ///////Filter///
+// // // // // // // // //                  //this method create new array with only element passed condition,,,,Filter always reurn in array.
 
-// // // //                 const arr=[1,2,3,4,5,6,7,8];
-// // // //                 const sum = arr.reduce((total, value) => total-value,2);
-// // // //                 console.log(sum);
+// // // // // // // //            const arr = [1,2,3,4,5,6,7,8,9];
+// // // // // // // //             const arrfilter = arr.filter(num => num<=6);
+// // // // // // // //             console.log(arrfilter);
 
 
+// // // // // // // // //////map///
+// // // // // // // //               //this method create new array by calling the providing function in every element.
 
-// // // /////Some///
-// // //              // this method check at least one of array item passsed the condition if return true otherwise fals.
-             
-// // //           const arr=[1,2,3,4,5,6,7,8,9];
-// // //           //at least one element >4? 
-// // //           const largeNum = arr.some(num => num>4);
-// // //           console.log(largeNum);  
+// // // // // // // //    //           const arr=[1,2,3,4,5,6,7,8,9];
+// // // // // // // //      //         const oneAdded = arr.map(num => num+2);
+// // // // // // // //        //       console.log(oneAdded);
+
+
+// // // // // // // //////Reduce///
+// // // // // // //                 // This method applies a function an accumlator and each element it the array from (left to right) to reduce it to a single value.
+
+// // // // // // //                 const arr=[1,2,3,4,5,6,7,8];
+// // // // // // //                 const sum = arr.reduce((total, value) => total-value,2);
+// // // // // // //                 console.log(sum);
 
 
 
-// // //////Every////
-// //                 //This method check all array item passed the condition if passed it return true otherwisw fals.
+// // // // // // /////Some///
+// // // // // //              // this method check at least one of array item passsed the condition if return true otherwise fals.
 
-// //              const arr=[1,2,3,4,5,6,7,8,9];
-// //              //all element greaterthan fuor 4//   
-// //              const greaterFour= arr.every(num => num>4);
-// //              console.log(greaterFour);
-             
-// //              const lessTen= arr.every(num => num<10);
-// //              console.log(lessTen);
+// // // // // //           const arr=[1,2,3,4,5,6,7,8,9];
+// // // // // //           //at least one element >4? 
+// // // // // //           const largeNum = arr.some(num => num>4);
+// // // // // //           console.log(largeNum);  
 
 
-// //Sort///
 
-// //This method used to arrange sort array item eitheir assanding or dessanding order,,,
+// // // // // //////Every////
+// // // // //                 //This method check all array item passed the condition if passed it return true otherwisw fals.
 
-//    const arr=[1,2,3,4,5,6,7,8];
-//     //sort in dassanding order//
+// // // // //              const arr=[1,2,3,4,5,6,7,8,9];
+// // // // //              //all element greaterthan fuor 4//   
+// // // // //              const greaterFour= arr.every(num => num>4);
+// // // // //              console.log(greaterFour);
 
-//     descOrder=arr.sort((a,b)=> a > b ? -1:1);
-//     console.log(descOrder);
-
-//     //sort in assanding order//
-//     aescOrder=arr.sort((a,b)=> a > b ? 1:-1);
-//             console.log(aescOrder);
+// // // // //              const lessTen= arr.every(num => num<10);
+// // // // //              console.log(lessTen);
 
 
-//Array form//
+// // // // //Sort///
 
-              //This method change all thing in array,,
+// // // // //This method used to arrange sort array item eitheir assanding or dessanding order,,,
 
-              const name=["Amjadkhan"];
-              const nameArray=Array.form(Array);
-              console.log(nameArray);
+// // // //    const arr=[1,2,3,4,5,6,7,8];
+// // // //     //sort in dassanding order//
+
+// // // //     descOrder=arr.sort((a,b)=> a > b ? -1:1);
+// // // //     console.log(descOrder);
+
+// // // //     //sort in assanding order//
+// // // //     aescOrder=arr.sort((a,b)=> a > b ? 1:-1);
+// // // //             console.log(aescOrder);
+
+
+// // // //Array form//
+
+// // // //This method change all thing in array,,
+
+// // // //   const name=["Amjadkhan"];
+// // // //   const nameArray=Array.form(Array);
+// // // //   console.log(nameArray);
+
+// // // //index of//
+
+// const marks = [10, 20, 30, 40, 50, 60, 80];
+
+// // // //check the index of element in array
+// const s = marks.indexOf(20);
+//  console.log(s);
+
+
+// // console.log("amjad")
+// // console.log("khan")
+
+
+// //const marks = [10, 20, 30, 40, 50, 60, 80];
+// //const a =marks.indexOf(80);
+// //console.log(a);
+
+
